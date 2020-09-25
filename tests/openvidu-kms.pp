a77b34953e033bcd5955343d77c38a7dff83585b
Antonio Alisio de Meneses Cordeiro <alisio.meneses@gmail.com>
2020-09-25 09:43:41 -0300

    Rename kms test manifest

--- tests/openvidu-kms.pp
+++ tests/openvidu-kms.pp
@@ -1,7 +1,7 @@
 # This manifest is intended to be used with the included vagrant file
 class {'openvidu':
   public_ip          => '192.168.33.10',
-  kms_uris           => '["ws://192.168.33.11:8888/kurento"]',
+  kms_uris           => '["ws://127.0.0.1:8888/kurento"]',
   network_interfaces => 'enp0s8',
   secret             => 'secret',
 }
